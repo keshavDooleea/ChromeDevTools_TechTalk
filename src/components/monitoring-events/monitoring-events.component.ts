0ec67a9677b185f0c1954be5ac052d07f6b1cf70
keshav <kdooleea@yahoo.ca>
2024-07-09 20:57:47 -0400

    feat: added Test button for console.log

--- src/components/monitoring-events/monitoring-events.component.ts
+++ src/components/monitoring-events/monitoring-events.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-monitoring-events',
   templateUrl: './monitoring-events.component.html',
-  styleUrls: ['./monitoring-events.component.css']
+  styleUrls: ['./monitoring-events.component.css'],
 })
 export class MonitoringEventsComponent {
-
+  onTestClicked(event: MouseEvent): void {
+    console.log(event);
+  }
 }
