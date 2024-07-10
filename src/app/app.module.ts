e5e58ce0564f274b1c7ce8d01e333c6f0e8447f3
keshav <kdooleea@yahoo.ca>
2024-07-09 23:27:20 -0400

    feat: added css section for specificity

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { SpinnerComponent } from '../components/spinner/spinner.component';
 import { MonitoringEventsComponent } from '../components/monitoring-events/monitoring-events.component';
 import { SnapScrollComponent } from '../components/snap-scroll/snap-scroll.component';
 import { IntroComponent } from '../components/intro/intro.component';
+import { CssComponent } from '../components/css/css.component';
 
 @NgModule({
-  declarations: [AppComponent, ProfilingComponent, SpinnerComponent, MonitoringEventsComponent, SnapScrollComponent, IntroComponent],
+  declarations: [AppComponent, ProfilingComponent, SpinnerComponent, MonitoringEventsComponent, SnapScrollComponent, IntroComponent, CssComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
